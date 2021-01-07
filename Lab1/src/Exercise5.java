be93e3982aefee5c25222b1c39a62ca69d9a1b3a
Rishita Choubey <46817010+rishitachoubey@users.noreply.github.com>
2021-01-07 16:16:01 +0530

    Update Exercise5.java

--- Lab1/src/Exercise5.java
+++ Lab1/src/Exercise5.java
@@ -1,3 +1,9 @@
+** 
+ * Create a class with a method which can calculate the sum of first n natural numbers which are divisible by 3 or 5.
+ * @author RISHITA CHOUBEY
+ *
+ */
+
 
 public class Exercise5 {
 
