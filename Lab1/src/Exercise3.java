eb7a8581bb519a2a88a162011818ee51b525bdf2
Rishita Choubey <46817010+rishitachoubey@users.noreply.github.com>
2021-01-07 16:14:26 +0530

    Update Exercise3.java

--- Lab1/src/Exercise3.java
+++ Lab1/src/Exercise3.java
@@ -1,3 +1,10 @@
+
+/** 
+ * The Fibonacci sequence is defined by the following rule. The first 2 values in the sequence are 1, 1. Every subsequent value is the sum of the 2 values preceding it. Write a Java program that uses both recursive and non-recursive functions to print the nth value of the Fibonacci sequence?.
+ * @author RISHITA CHOUBEY
+ *
+ */
+
 import java.util.*;
 public class Exercise3
 {
